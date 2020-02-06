307751b512037f9ad3bbc58d0a7522222a004b2f
Vlado Cavric <vladocavric@gmail.com>
2020-02-06 16:53:18 +0100

    added some tmplates

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ app.use(express.static('themes'));
 app.use(bodyParser.urlencoded({extend: true}));
 app.set('view engine', 'ejs');
 
+var pizzeria = [
+    {name: 'Panter', img: 'https://panterns.com/wp-content/uploads/2017/02/cropped-logo-1-1.png'},
+    {name: 'BigPizza', img:'https://bigpizza.rs/images/logo.png' },
+    {name: 'laTorcia', img:'https://cafferestoranlatorcia.com/images/logo.png'},
+    {name: 'Ciao', img: 'http://pizzeriaciao.rs/media/1118/pizzeria_ciao_logo.svg'},
+    {name: 'San Francisco', img: 'http://www.sfpizza.rs/images/sf-logo-200-transparent.png'} 
+]
+
 app.get('/', function(req, res){
     res.render('Home')
 })
 
-app.get('/camphrounds', function(req, res){
-    res.render('Camphrounds')
+app.get('/pizzeria', function(req, res){
+    res.render('pizzeria', {pizzeria: pizzeria})
 })
 
 app.get('*', function(req, res){
