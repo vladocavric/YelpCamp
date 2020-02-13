5a784179f0444d1c86aff5fca67ed64d078530ab
Vlado Cavric <vladocavric@gmail.com>
2020-02-13 16:26:05 +0100

    finished pizza app

--- app.js
+++ app.js
@@ -3,36 +3,73 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 const rp = require('request-promise');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/pizzerias', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static('themes'));
 app.use(bodyParser.urlencoded({extend: true}));
 app.set('view engine', 'ejs');
 
-let pizzeria = [
-    {name: 'Panter', img: 'https://panterns.com/wp-content/uploads/2017/02/cropped-logo-1-1.png'},
-    {name: 'BigPizza', img: 'https://bigpizza.rs/images/logo.png'},
-    {name: 'laTorcia', img: 'https://cafferestoranlatorcia.com/images/logo.png'},
-    {name: 'Ciao', img: 'http://pizzeriaciao.rs/media/1118/pizzeria_ciao_logo.svg'},
-    {name: 'San Francisco', img: 'http://www.sfpizza.rs/images/sf-logo-200-transparent.png'}
-];
+let pizzeria = mongoose.model('pizzeria', {
+    name: String,
+    img: String,
+    description: String
+});
 
-pizzeria.reverse();
+// pizzeria.create({
+//     name: 'Pizza Hut',
+//     img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.visitgalax.com%2Fproject%2Fpizza-hut%2F&psig=AOvVaw0JccEcr5G4wVtduxpeviTe&ust=1581673896807000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJiJ4cigzucCFQAAAAAdAAAAABAD'
+// }, function (err, pizzeria) {
+//     if(err){
+//         console.log(err);
+//         console.log('something went wrong');
+//     } else {
+//         console.log('new pizzaria')
+//     }
+// });
+
+// let pizzeria = [
+//     {name: 'Panter', img: 'https://panterns.com/wp-content/uploads/2017/02/cropped-logo-1-1.png'},
+//     {name: 'BigPizza', img: 'https://bigpizza.rs/images/logo.png'},
+//     {name: 'laTorcia', img: 'https://cafferestoranlatorcia.com/images/logo.png'},
+//     {name: 'Ciao', img: 'http://pizzeriaciao.rs/media/1118/pizzeria_ciao_logo.svg'},
+//     {name: 'San Francisco', img: 'http://www.sfpizza.rs/images/sf-logo-200-transparent.png'}
+// ];
+
+// pizzeria.reverse();
 
 app.get('/', function (req, res) {
     res.render('Home')
 });
 
 app.get('/pizzeria', function (req, res) {
-
-    res.render('pizzeria', {pizzeria: pizzeria})
+    pizzeria.find({}, function (err, pizzerias) {
+        if(err){
+            console.log('oh no error');
+            console.log(err);
+        } else {
+            console.log('all the pizzas');
+            res.render('index', {pizzerias: pizzerias})
+        }
+    })
 });
 
 app.post('/pizzeria', function (req, res) {
     const name = req.body.pizzeria;
     const img = req.body.url;
-    let newPizzeria = {name: name, img: img};
-    pizzeria.unshift(newPizzeria);
-    // res.send('you are on post rout')
+    const description = req.body.description;
+    pizzeria.create({
+        name: name,
+        img: img,
+        description: description
+    }, function (err, pizzeria) {
+        if(err){
+            console.log(err);
+        } else {
+            console.log(pizzeria);
+        }
+    });
     res.redirect('pizzeria')
 });
 
@@ -40,9 +77,20 @@ app.get('/pizzeria/new', function (req, res) {
     res.render('newpizzeria')
 });
 
+app.get('/pizzeria/:id', function (req, res) {
+    let id = req.params.id;
+    pizzeria.findById(id, function (err, foundPizza) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('show', {pizza: foundPizza})
+        }
+    });
+});
+
 app.get('*', function (req, res) {
     res.send('404');
 });
 
 
-app.listen(8012);
+app.listen(8015);
